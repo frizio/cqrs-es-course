9c7d3731eb55db0a407fa393224fe2d02848e892
Maurizio La Rocca <maurizio.larocca@exprivia.com>
2022-05-10 20:24:18 +0200

    Basic project setup

---
+++ bank-account/account.cmd/src/main/java/xyz/frizio/techbank/account/cmd/CommandApplication.java
@@ -0,0 +1,13 @@
+package xyz.frizio.techbank.account.cmd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommandApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CommandApplication.class, args);
+    }
+
+}
---
+++ bank-account/account.query/src/main/java/xyz/frizio/techbank/account/query/QueryApplication.java
@@ -0,0 +1,13 @@
+package xyz.frizio.techbank.account.query;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QueryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QueryApplication.class, args);
+	}
+
+}
